dd6073b53b3a945627297d88e7b736d1261852e1
skyclouds-night <jcmjalquitran@gmail.com>
2025-03-28 17:10:12 +0800

    Add files via upload

---
+++ timer_countdown.py
@@ -0,0 +1,12 @@
+import time
+def timer(t):
+    while t: 
+        mins, secs = divmod(t, 60) 
+        timer = '{:02d}:{:02d}'.format(mins, secs) 
+        print(timer, end="\r") 
+        time.sleep(1) 
+        t -= 1
+        print("Time's Up!")
+        break
+return timer
+
